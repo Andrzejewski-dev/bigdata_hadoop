86f514a594f0967eae5b358d36310e5f441b0a0d
Kamil Andrzejewski <kamil.andrzejewski@mug.pl>
2020-05-10 00:35:34 +0200

    fix

--- run.sh
+++ run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+rm -rf project
+hadoop fs -rm -rf project
+
 mkdir project
 hadoop fs -mkdir -p project
 
 mkdir project/tripdata
-mkdir project/output1
-
 hadoop fs -mkdir -p project/tripdata
+
+mkdir project/output1
 hadoop fs -mkdir -p project/output1
 
-wget https://raw.githubusercontent.com/Andrzejewski-dev/bigdata_hadoop/master/passengerperarea.jar project/passengerperarea.jar
+wget https://raw.githubusercontent.com/Andrzejewski-dev/bigdata_hadoop/master/passengerperarea.jar -P project/passengerperarea.jar
 
 wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2019-01.csv -P project/tripdata/yellow_tripdata_2019-01.csv
 wget https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2019-02.csv -P project/tripdata/yellow_tripdata_2019-01.csv
@@ -28,18 +31,18 @@ wget https://s3.amazonaws.com/nyc-tlc/misc/taxi+_zone_lookup.csv -P project/taxi
 
 hadoop fs -copyFromLocal project/passengerperarea.jar project/passengerperarea.jar
 hadoop fs -copyFromLocal project/taxi_zone_lookup.csv project/taxi_zone_lookup.csv
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-01.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-02.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-03.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-04.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-05.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-06.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-07.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-08.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-09.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-10.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-11.csv project/tripdata/
-hadoop fs -copyFromLocal project/tripdata/tripdata/yellow_tripdata_2019-12.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-01.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-02.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-03.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-04.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-05.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-06.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-07.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-08.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-09.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-10.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-11.csv project/tripdata/
+hadoop fs -copyFromLocal project/tripdata/yellow_tripdata_2019-12.csv project/tripdata/
 
 hadoop jar project/passengerperarea.jar PassengerPerArea project/tripdata/*.csv project/output1
 
